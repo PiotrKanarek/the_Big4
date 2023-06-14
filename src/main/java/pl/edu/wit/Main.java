7bd196441be38d2e9529afd0f66d8f406833724a
BartlomiejKilim <bkilim@gmail.com>
2023-06-14 14:35:16 +0200

    bla bla bla krasnale

--- src/main/java/pl/edu/wit/Main.java
+++ src/main/java/pl/edu/wit/Main.java
@@ -1,37 +1,34 @@
 package pl.edu.wit;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import pl.edu.wit.config.PropertySource;
-import pl.edu.wit.config.ExecutorConfigurator;
-import pl.edu.wit.file.FileCopyingService;
-
-import java.util.concurrent.ExecutorService;
 
 public class Main {
+	
+	private static final Logger log = LogManager.getLogger(MainVBK.class.getName());
 
-    private static final Logger log = LogManager.getLogger(Main.class.getName());
-
-    public static void main(String[] args) {
-
-        log.info("Application started");
-
-        // TODO: front-end + walidacja
-        String sourceDirectory = "tu dajcie ścieżkę do folderu ze zdjęciami do przekopiowania";
-        String destinationDirectory = "a tu ścieżkę do folderu do którego mają być przekopiowane pliki";
-        // albo przekażcie je bezpośrednio do metody poniżej, to tylko wstępny setup
-
-        PropertySource propertySource = new PropertySource("src/main/resources/application.properties");
-        ExecutorService executorService = ExecutorConfigurator.getConfiguredExecutor(propertySource);
-
-        int filesCopied = 0;
-        try (FileCopyingService copyingService = new FileCopyingService(executorService)) {
-            filesCopied = copyingService.copyFiles(sourceDirectory, destinationDirectory);
-        } catch (RuntimeException e) {
-            log.error("Exception occurred while copying the files: " + e.getMessage());
-        }
-
-        log.info("Processing finished - copied " + filesCopied + " files");
-    }
-
+	public static void main(String[] args){
+	
+		log.info("Application started");
+		try {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
+			log.error(e.getMessage());
+		} catch (IllegalAccessException e) {
+			log.error(e.getMessage());
+		} catch (InstantiationException e) {
+			log.error(e.getMessage());
+		} catch (ClassNotFoundException e) {
+			log.error(e.getMessage());
+		}
+		//GUI object. It's constructor launches main frame creation and executes functionalities
+		// according to event driven logic.
+		FilesGUI gui = new FilesGUI();
+		gui.getFrame().setVisible(true);
+		gui.getFrame().setLocationRelativeTo(null);
+	}
 }
+
